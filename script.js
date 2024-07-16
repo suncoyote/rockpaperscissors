3b5d8314b25bbd8b7fc8f6a4c51722de2283fb36
Aspen <175189330+suncoyote@users.noreply.github.com>
2024-07-16 02:26:45 -0400

    Add function code to convert numbers into choices 1,2 and 3 now stand for "rock" "paper" and "scissors"

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 
 function getComputerChoice() {
- return Math.floor(Math.random()*(3 -0) + 1);
+ var choiceNumber = Math.floor(Math.random()*(3 -0) + 1);
+
+ if (choiceNumber == 1) { 
+    return "Rock"
+
+ }else if (choiceNumber == 2){ 
+    return "Paper"
+
+ }else { 
+    return "Scissors"
+    
+ }
+
 }
 
 var computerChoice = getComputerChoice()
